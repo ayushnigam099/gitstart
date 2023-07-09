c2929a851a932a31f8946ee3562bc7aef07d8647
ayushnigam099 <ayushnigam95531@gmail.com>
2023-07-09 15:00:03 +0530

    added color,border and also styled h1 tag

--- demo.js
+++
@@ -1,3 +0,0 @@
-console.log("Hello this side ayush nigam")
-console.log("Hello this side ayush nigam from sharpener")
-
---
+++ dom.js
@@ -0,0 +1,17 @@
+// console.dir(document);
+// console.dir(document.domain);
+
+// console.log(document.title)
+// console.log(document.title=123);
+let item = document.querySelector('.title');
+console.log(item);
+
+let header= document.getElementById('header-title');
+let headerparent= document.getElementById('main-header');
+headerparent.style.border="2px solid black";
+console.log(header);
+header.style.color="pink";
+item.style.color="green";
+// item.style.borderBottom="4px solid orange";
+item.style.fontWeight= "bold";
+header.style.border="5px solid pink";
