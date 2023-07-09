0cac1b87e4d4b74294bc6e8c4e0a9bd699a6eeef
ayushnigam099 <ayushnigam95531@gmail.com>
2023-07-09 16:47:13 +0530

    added color,border and also styled h1 tag

--- dom.js
+++ dom.js
@@ -3,15 +3,15 @@
 
 // console.log(document.title)
 // console.log(document.title=123);
-let item = document.querySelector('.title');
-console.log(item);
+let item = document.getElementsByClassName('title');
+console.log(item[0]);
 
 let header= document.getElementById('header-title');
 let headerparent= document.getElementById('main-header');
 headerparent.style.border="2px solid black";
 console.log(header);
 header.style.color="pink";
-item.style.color="green";
+item[0].style.color="green";
 // item.style.borderBottom="4px solid orange";
-item.style.fontWeight= "bold";
+item[0].style.fontWeight= "bold";
 header.style.border="5px solid pink";
